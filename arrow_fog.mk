060432ecf2099da22a628b024facc80f82ab107b
zidnz0 <zaidaansaad01@gmail.com>
2026-07-11 20:53:36 +0700

    Update arrow_fog.mk

--- arrow_fog.mk
+++ arrow_fog.mk
@@ -9,15 +9,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-TARGET_BOOT_ANIMATION_RES := 720
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_QUICK_TAP := false
+$(call inherit-product, vendor/arrow/config/common_full_phone.mk)
 
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
 
-PRODUCT_NAME := lineage_fog
+# Arrow flags
+DEVICE_MAINTAINER := Zidnz0
+DEVICE_MAINTAINER_LINK := https://t.me/zidnz0
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := false
+
+PRODUCT_NAME := arrow_fog
 PRODUCT_DEVICE := fog
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
