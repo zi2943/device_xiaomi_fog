5b680567e54a96b2765f2ab338b7f6d7501f53d3
zidnz0 <zaidaansaad01@gmail.com>
2026-07-11 20:54:08 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_fog.mk
+    $(LOCAL_DIR)/arrow_fog.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_fog-user \
-    lineage_fog-userdebug \
-    lineage_fog-eng
+    arrow_fog-user \
+    arrow_fog-userdebug \
+    arrow_fog-eng
