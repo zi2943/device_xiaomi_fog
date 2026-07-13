160ab99fa77d82e75c9d396a255930f000e6ad0f
zidnz0 <zaidaansaad01@gmail.com>
2026-07-13 17:01:11 +0700

    Update arrow_fog.mk

--- arrow_fog.mk
+++ arrow_fog.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/arrow/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
