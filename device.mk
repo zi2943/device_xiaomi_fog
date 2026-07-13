6fbe6152f69b2cb5d29266ee09537ab2f0bed520
zidnz0 <zaidaansaad01@gmail.com>
2026-07-13 17:55:07 +0700

    Drop miui camera

--- device.mk
+++ device.mk
@@ -555,5 +555,3 @@ PRODUCT_BOOT_JARS += \
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/fog/fog-vendor.mk)
 
-# Miui Camera
-$(call inherit-product, vendor/xiaomi/miuicamera/MiuiCamera.mk)
