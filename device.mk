d1ee293e3718a7094810ff35540ee5ed22568603
zidnz0 <zaidaansaad01@gmail.com>
2026-07-28 22:56:57 +0700

    include Dolby Atmos

--- device.mk
+++ device.mk
@@ -174,6 +174,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     gralloc.qcom
 
+# Dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
+
+PRODUCT_PACKAGES += \
+    LunarisDolby
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
