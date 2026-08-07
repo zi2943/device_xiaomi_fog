aff8e203ffde9dae5aadcf5495d922bee00cb9c4
zidnz0 <zaidaansaad01@gmail.com>
2026-08-07 21:39:25 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/infinity_fog.mk
+    $(LOCAL_DIR)/lineage_fog.mk
 
 COMMON_LUNCH_CHOICES := \
-    infinity_fog-user \
-    infinity_fog-userdebug \
-    infinity_fog-eng
+    lineage_fog-user \
+    lineage_fog-userdebug \
+    lineage_fog-eng
