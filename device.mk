de1d7ca5a70f6acd174eed7e9b637492dd72b63b
zidnz0 <zaidaansaad01@gmail.com>
2026-08-07 21:10:23 +0700

    Include miuicamera

--- device.mk
+++ device.mk
@@ -592,3 +592,6 @@ PRODUCT_PACKAGES += \
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/fog/fog-vendor.mk)
+
+# Inherit from vendor Miui Camera
+$(call inherit-product, vendor/xiaomi/camera/miuicamera.mk)
