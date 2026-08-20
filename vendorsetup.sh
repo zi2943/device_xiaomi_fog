775de4f00b1d6b021c04f9a09b3a2155dee397c9
zidnz0 <zaidaansaad01@gmail.com>
2026-08-20 18:57:39 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,2 @@
 # Remove Pixel kernel header
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
-
-# Clone vendor tree
-git clone -b fourteen-refresh https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
-
-# Clone prebuilt kernel
-git clone https://github.com/alternoegraha/device_xiaomi_fog-kernel device/xiaomi/fog-kernel
-
-# Clone LineageOS' hardware/xiaomi
-git clone -b lineage-21 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
